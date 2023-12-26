3ea1a9fb723d49b5aee4c460ef18d727f811d99b
Miguel Albaladejo <malbaladejo@Miguels-Mac-mini.local>
2023-12-26 18:50:48 +0100

    Introduced supressed warning

--- src/main/java/com/miguel/DependencyCounterMojo.java
+++ src/main/java/com/miguel/DependencyCounterMojo.java
@@ -9,10 +9,6 @@ import org.apache.maven.project.MavenProject;
 
 import java.util.List;
 
-/**
- * Implementation of the dependency counter Mojo described
- * in the baeldung article <a href="https://www.baeldung.com/maven-plugin">...</a>
- */
 @Mojo(name = "dependency-counter", defaultPhase = LifecyclePhase.COMPILE)
 public class DependencyCounterMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
@@ -23,6 +19,7 @@ public class DependencyCounterMojo extends AbstractMojo {
 
     @Override
     public void execute() {
+        @SuppressWarnings("unchecked")
         List<Dependency> dependencies = project.getDependencies();
         long numDependencies = dependencies.stream().filter(d -> (scope == null || scope.isEmpty()) || scope.equals(d.getScope())).count();
         getLog().info("Number of dependencies: " + numDependencies);

eea5081f10a9b4da0b250085dd9c412821448ea6
Miguel Albaladejo <malbaladejo@okode.com>
2023-12-26 18:33:35 +0100

    Introduced parameter for hello-world plugin

--- src/main/java/com/miguel/HelloWorldMojo.java
+++ src/main/java/com/miguel/HelloWorldMojo.java
@@ -3,12 +3,18 @@ package com.miguel;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.commons.lang3.StringUtils;
 
 @Mojo(name = "hello-world", defaultPhase = LifecyclePhase.COMPILE)
 public class HelloWorldMojo extends AbstractMojo {
 
+    @Parameter(property = "name")
+    private String name;
+
     @Override
     public void execute() {
-        getLog().info("Hello World!");
+        var message = StringUtils.isBlank(name) ? "World" : name;
+        getLog().info("Hello, " + message + "!");
     }
 }
